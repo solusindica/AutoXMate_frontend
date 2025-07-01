e34ed194d26787c050dce000636f234e2e0ab91a
solusindica <drprs@sgprs.com>
2025-07-01 05:14:29 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+ 
   plugins: [react()],
-  base: mode === 'production' ? '/AutoXMate_frontend/' : '/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-}));
+});
